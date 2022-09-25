6f71073a0af1794c98f2bb5005951f3178fb50ad
sfcc5555 <sfcc.5555@gmail.com>
2022-09-25 04:49:23 -0500

    Actualizacion maunScreen

--- static/GALERIA/galeria.js
+++ static/GALERIA/galeria.js
@@ -1,13 +1,9 @@
-let b1= document.querySelector("button")
-let a1= document.getElementById("a");
 
-b1.addEventListener("click", () => {
-    a1.setAttribute("src", "/static/GALERIA/AUDIO/THANKYOU.mp3")
-    a1.play()
-})
+
 
 function audio()
 {
-    a1.setAttribute("src", "/static/GALERIA/AUDIO/THANKYOU.mp3")
-    a1.play()
+    a= document.createElement("audio");
+    a.setAttribute("src", "/static/GALERIA/AUDIO/THANKYOU.mp3");
+    a.play();
 }
